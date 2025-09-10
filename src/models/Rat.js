cf00ca1d42c857a9ad0795b0165cc9013a73e804
Chase McLain <chasermclain@gmail.com>
2025-09-10 14:05:57 -0600

    rat schema

--- src/models/Rat.js
+++ src/models/Rat.js
@@ -4,9 +4,16 @@ import { Schema } from "mongoose";
 
 export const RatSchema = new Schema({
 
+  name: { type: String, required: true, minLength: 1, maxLength: 25 },
+  callsign: { type: String, required: true, minLength: 1, maxLength: 50 },
+  picture: { type: String, required: true, minLength: 1, maxLength: 500 },
+  specialities: [{ type: String, required: true }],
 
+},
 
-})
+  { versionKey: false, timestamps: true, toJSON: { virtuals: true } }
+
+)
 
 
 
